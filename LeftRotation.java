68fcdaf40f28ce522a3c86c9c589f0ca852aa62f
SunilNavgurukul <sunil18@navgurukul.org>
2020-09-15 23:28:24 +0530

    4 questions of array and arrayList and one fromtreeMap

---
+++ LeftRotation.java
@@ -0,0 +1,32 @@
+package com.practice.hackkerank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LeftRotation {
+	public static List<Integer> rotateLeft(int d, List<Integer> arr) {
+		List<Integer> rotetatedList = new ArrayList<Integer>();
+		for (int i = d; i < arr.size(); i++) {
+			rotetatedList.add(arr.get(i));
+		}
+		for (int j = 0; j < d; j++) {
+			rotetatedList.add(arr.get(j));
+		}
+
+		for (int p : rotetatedList)
+			System.out.println(p);
+		return rotetatedList;
+
+	}
+
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(5);
+		rotateLeft(4, list);
+	}
+
+}
---
+++ Solution.java
@@ -0,0 +1,43 @@
+package com.practice.hackkerank;
+
+//import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+public class Solution {
+
+	static SortedMap<Character, Integer> counter(String word) {
+		SortedMap<Character, Integer> sm = new TreeMap<Character, Integer>();
+		for (int i = 0; i < word.length(); i++) {
+			if(Character.isUpperCase(word.charAt(i)) || Character.isLowerCase(word.charAt(i))) {
+				sm.put(Character.toLowerCase(word.charAt(i)), 0);
+			}
+		}
+		for (int i = 0; i < word.length(); i++) {
+			if(Character.isUpperCase(word.charAt(i)) || Character.isLowerCase(word.charAt(i))) {
+				sm.put(Character.toLowerCase(word.charAt(i)), sm.get(Character.toLowerCase(word.charAt(i))) + 1);
+			}
+		}
+		return sm;
+	}
+
+	static boolean isAnagram(String a, String b) {
+		if (a.length() == b.length()) {
+			return counter(a).equals(counter(b));
+
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		String a = scan.next();
+		String b = scan.next();
+		scan.close();
+		boolean ret = isAnagram(a, b);
+		System.out.println((ret) ? "Anagrams" : "Not Anagrams");
+	}
+}
